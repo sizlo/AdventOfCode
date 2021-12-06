2e0e93712951c814c8bc2027860ea430af27b1f9
Tim Brier <t.brier93@gmail.com>
2021-12-06 14:24:42 +0000

    Solve 2021 day05

---
+++ 2021/src/main/kotlin/advent/of/code/day05/HydrothermalVenture.kt
@@ -0,0 +1,73 @@
+package advent.of.code.day05
+
+import advent.of.code.utils.readInput
+import advent.of.code.utils.toIntList
+import kotlin.math.abs
+
+class HydrothermalVenture(input: List<String>) {
+
+    private val lines = input.map(::parseLine)
+
+    data class Coordinate(val x: Int, val y: Int)
+
+    class Line(private val endpoints: Pair<Coordinate, Coordinate>) {
+
+        fun getCoveredCoordinates(): List<Coordinate> {
+            val dx = calculateDelta(endpoints.first.x, endpoints.second.x)
+            val dy = calculateDelta(endpoints.first.y, endpoints.second.y)
+
+            var x = endpoints.first.x
+            var y = endpoints.first.y
+
+            val coordinates = mutableListOf<Coordinate>()
+
+            while (!coordinates.contains(endpoints.second)) {
+                coordinates.add(Coordinate(x, y))
+                x += dx
+                y += dy
+            }
+
+            return coordinates
+        }
+
+        private fun calculateDelta(first: Int, second: Int): Int {
+            if (first == second) return 0
+            val difference = second - first
+            return difference / abs(difference)
+        }
+
+        fun isHorizontal(): Boolean = endpoints.first.y == endpoints.second.y
+        fun isVertical(): Boolean = endpoints.first.x == endpoints.second.x
+    }
+
+    fun countPointsCoveredByNonDiagonalLines(minimumOverlap: Int): Int {
+        val nonDiagonalLines = lines.filter { it.isHorizontal() || it.isVertical() }
+        return countPointsCoveredByLines(nonDiagonalLines, minimumOverlap)
+    }
+
+    fun countPointsCoveredByLines(minimumOverlap: Int): Int {
+        return countPointsCoveredByLines(lines, minimumOverlap)
+    }
+
+    private fun countPointsCoveredByLines(lines: List<Line>, minimumOverlap: Int): Int {
+        return lines
+            .flatMap(Line::getCoveredCoordinates)
+            .groupBy { it }
+            .count { it.value.size >= minimumOverlap }
+    }
+
+    private fun parseLine(string: String): Line {
+        val coordinates = string
+            .split(" -> ")
+            .map { it.split(",").toIntList() }
+            .map { Coordinate(it[0], it[1]) }
+        return Line(Pair(coordinates[0], coordinates[1]))
+    }
+}
+
+fun main() {
+    val input = readInput("/day05/input.txt")
+    val hydrothermalVenture = HydrothermalVenture(input)
+    println(hydrothermalVenture.countPointsCoveredByNonDiagonalLines(minimumOverlap = 2)) // 5698
+    println(hydrothermalVenture.countPointsCoveredByLines(minimumOverlap = 2)) // 15463
+}
---
+++ 2021/src/test/kotlin/advent/of/code/day05/HydrothermalVentureTest.kt
@@ -0,0 +1,91 @@
+package advent.of.code.day05
+
+import advent.of.code.day05.HydrothermalVenture.Coordinate
+import advent.of.code.day05.HydrothermalVenture.Line
+import assertk.assertThat
+import assertk.assertions.containsExactly
+import assertk.assertions.isEqualTo
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+internal class HydrothermalVentureTest {
+
+    private val exampleInput = listOf(
+        "0,9 -> 5,9",
+        "8,0 -> 0,8",
+        "9,4 -> 3,4",
+        "2,2 -> 2,1",
+        "7,0 -> 7,4",
+        "6,4 -> 2,0",
+        "0,9 -> 2,9",
+        "3,4 -> 1,4",
+        "0,0 -> 8,8",
+        "5,5 -> 8,2",
+    )
+
+    private val testSubject = HydrothermalVenture(exampleInput)
+
+    @Test
+    fun `test example input for part 1`() {
+        val result = testSubject.countPointsCoveredByNonDiagonalLines(minimumOverlap = 2)
+
+        assertThat(result).isEqualTo(5)
+    }
+
+    @Test
+    fun `test example input for part 2`() {
+        val result = testSubject.countPointsCoveredByLines(minimumOverlap = 2)
+
+        assertThat(result).isEqualTo(12)
+    }
+
+    @Nested
+    inner class LineTest {
+
+        @Test
+        fun `gets all covered coordinates for horizontal line`() {
+            val line = Line(Pair(Coordinate(1,5), Coordinate(6,5)))
+
+            val result = line.getCoveredCoordinates()
+
+            assertThat(result).containsExactly(
+                Coordinate(1, 5),
+                Coordinate(2, 5),
+                Coordinate(3, 5),
+                Coordinate(4, 5),
+                Coordinate(5, 5),
+                Coordinate(6, 5)
+            )
+        }
+
+        @Test
+        fun `gets all covered coordinates for vertical line`() {
+            val line = Line(Pair(Coordinate(1,5), Coordinate(1,10)))
+
+            val result = line.getCoveredCoordinates()
+
+            assertThat(result).containsExactly(
+                Coordinate(1, 5),
+                Coordinate(1, 6),
+                Coordinate(1, 7),
+                Coordinate(1, 8),
+                Coordinate(1, 9),
+                Coordinate(1, 10)
+            )
+        }
+
+        @Test
+        fun `gets all covered coordinates for diagonal line`() {
+            val line = Line(Pair(Coordinate(4,5), Coordinate(1,2)))
+
+            val result = line.getCoveredCoordinates()
+
+            assertThat(result).containsExactly(
+                Coordinate(4, 5),
+                Coordinate(3, 4),
+                Coordinate(2, 3),
+                Coordinate(1, 2),
+            )
+        }
+    }
+}
