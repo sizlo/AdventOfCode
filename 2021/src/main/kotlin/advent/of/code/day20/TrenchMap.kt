700040e7e1a5f4611fb8c63fdfd3e519b6eedc28
Tim Brier <t.brier93@gmail.com>
2021-12-20 07:54:10 +0000

    Simplify

--- 2021/src/main/kotlin/advent/of/code/day20/TrenchMap.kt
+++ 2021/src/main/kotlin/advent/of/code/day20/TrenchMap.kt
@@ -4,7 +4,7 @@ import advent.of.code.utils.*
 
 class TrenchMap {
 
-    enum class PixelBit(val char: Char, val bit: Int) {
+    enum class Pixel(val char: Char, val bit: Int) {
         LIGHT('#', 1), DARK('.', 0);
 
         companion object {
@@ -13,20 +13,16 @@ class TrenchMap {
         }
     }
 
-    class Pixel(coordinate: Coordinate, image: Image, val pixelValue: Int): GridItem<Pixel>(coordinate, image) {
-        fun isLit() = pixelValue == PixelBit.LIGHT.bit
-    }
-
-    class Image(input: IntGrid, private val infiniteBorderPixel: PixelBit = PixelBit.DARK): Grid<Pixel>(input) {
+    class Image(input: IntGrid, private val infiniteBorderPixel: Pixel = Pixel.DARK): Grid<Pixel>(input) {
         init {
-            populate { coordinate, pixelValue -> Pixel(coordinate, this, pixelValue) }
+            populate { _, pixelBit -> Pixel.fromBit(pixelBit) }
         }
 
         fun countLitPixels(): Int {
-            return items.count { it.isLit() }
+            return items.count { it == Pixel.LIGHT }
         }
 
-        fun enhance(algorithm: List<Int>): Image {
+        fun enhance(algorithm: List<Pixel>): Image {
             val enhancedImageInput = (-1 .. height).map { y ->
                 (-1 .. width).map { x ->
                     enhancePixelAt(Coordinate(x, y), algorithm)
@@ -34,35 +30,34 @@ class TrenchMap {
             }
 
             val newInfiniteBorderPixel = when (infiniteBorderPixel) {
-                PixelBit.DARK -> PixelBit.fromBit(algorithm.first())
-                PixelBit.LIGHT -> PixelBit.fromBit(algorithm.last())
+                Pixel.DARK -> algorithm.first()
+                Pixel.LIGHT -> algorithm.last()
             }
 
             return Image(enhancedImageInput, newInfiniteBorderPixel)
         }
 
-        private fun enhancePixelAt(coordinate: Coordinate, algorithm: List<Int>): Int {
-            val binaryIndexFromWindow = coordinate
+        private fun enhancePixelAt(coordinate: Coordinate, algorithm: List<Pixel>): Int {
+            val indexFromWindow = coordinate
                 .getNeighbourCoordinates(includeDiagonals = true)
                 .let { it + coordinate }
                 .sortedBy { it.y * 3 + it.x }
-                .map { getItemAt(it)?.pixelValue ?: infiniteBorderPixel.bit }
+                .map { getItemAt(it) ?: infiniteBorderPixel }
+                .map { it.bit }
                 .joinToString("")
-            val indexFromWindow = binaryIndexFromWindow.toInt(2)
+                .toInt(2)
 
-            return algorithm[indexFromWindow]
+            return algorithm[indexFromWindow].bit
         }
     }
 
     fun countLitPixelsAfterNIterations(input: List<String>, iterations: Int): Int {
-        val algorithm = input[0].map { PixelBit.fromChar(it).bit }
-        val imageInput = input.subList(2, input.size).toIntGrid { PixelBit.fromChar(it).bit }
+        val algorithm = input[0].map { Pixel.fromChar(it) }
+        val imageInput = input.subList(2, input.size).toIntGrid { Pixel.fromChar(it).bit }
         val image = Image(imageInput)
 
         val enhancedImage = (0 until iterations)
-            .foldIndexed(image) { index, enhancedImage, _ ->
-                enhancedImage.enhance(algorithm)
-            }
+            .fold(image) { enhancedImage, _ -> enhancedImage.enhance(algorithm) }
 
         return enhancedImage.countLitPixels()
     }
