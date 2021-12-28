973ef6cd527c95a4f5cc009b139544ad07ed7e4f
Tim Brier <t.brier93@gmail.com>
2021-12-28 12:56:39 +0000

    Remove repetition

--- 2021/src/main/kotlin/advent/of/code/day25/SeaCucumberPuzzle.kt
+++ 2021/src/main/kotlin/advent/of/code/day25/SeaCucumberPuzzle.kt
@@ -2,6 +2,7 @@ package advent.of.code.day25
 
 import advent.of.code.utils.Coordinate
 import advent.of.code.utils.readInputLines
+import java.lang.RuntimeException
 
 class SeaCucumberPuzzle {
 
@@ -27,43 +28,39 @@ class SeaCucumberPuzzle {
                     Coordinate(x, y) to SeaCucumber.fromChar(char)
                 }
             }
-            .filter { it.second != null }
-            .associate { it.first to it.second!! }
+            .toMap()
+            .filter { it.value != null }
             .toMutableMap()
 
         private val width = input[0].length
         private val height = input.size
 
         fun step(): Boolean {
-            val eastMovements = seaCucumbers
-                .filter { it.value == SeaCucumber.EAST }
-                .mapValues { it.key.rightWithWrap() }
-                .filter { isEmptyAt(it.value) }
-
-            eastMovements.forEach { (oldCoordinate, newCoordinate) ->
-                seaCucumbers.remove(oldCoordinate)
-                seaCucumbers[newCoordinate] = SeaCucumber.EAST
-            }
-
-            val southMovements = seaCucumbers
-                .filter { it.value == SeaCucumber.SOUTH }
-                .mapValues { it.key.bottomWithWrap() }
-                .filter { isEmptyAt(it.value) }
-
-            southMovements.forEach { (oldCoordinate, newCoordinate) ->
-                seaCucumbers.remove(oldCoordinate)
-                seaCucumbers[newCoordinate] = SeaCucumber.SOUTH
-            }
-
-            return eastMovements.isNotEmpty() || southMovements.isNotEmpty()
+            val movedEast = applyMovements(SeaCucumber.EAST)
+            val movedSouth = applyMovements(SeaCucumber.SOUTH)
+            return movedEast || movedSouth
         }
 
-        private fun Coordinate.rightWithWrap(): Coordinate {
-            return this.copy(x = (this.x + 1) % width)
+        private fun applyMovements(direction: SeaCucumber): Boolean {
+            val movements = seaCucumbers
+                .filter { it.value == direction }
+                .mapValues { it.key.next() }
+                .filter { isEmptyAt(it.value) }
+
+            movements.forEach { (oldCoordinate, newCoordinate) ->
+                seaCucumbers.remove(oldCoordinate)
+                seaCucumbers[newCoordinate] = direction
+            }
+
+            return movements.isNotEmpty()
         }
 
-        private fun Coordinate.bottomWithWrap(): Coordinate {
-            return this.copy(y = (this.y + 1) % height)
+        private fun Coordinate.next(): Coordinate {
+            return when (seaCucumbers[this]) {
+                SeaCucumber.EAST -> this.copy(x = (this.x + 1) % width)
+                SeaCucumber.SOUTH -> this.copy(y = (this.y + 1) % height)
+                else -> throw RuntimeException()
+            }
         }
 
         private fun isEmptyAt(coordinate: Coordinate): Boolean {
